f18ebab8600f497197fe7be6a476558d3dd25be3
aryan42bit <aryankulkarni6969@gmail.com>
2024-08-26 21:44:13 +0530

    Create vowel or consonent.cpp

---
+++ vowel or consonent.cpp
@@ -0,0 +1,18 @@
+//23070123076
+//aryankulkarni//
+sing namespace std;
+int main()
+{
+    char v;
+    cout<<"Enter the alphabet: ";
+    cin>>v;
+
+    if(v == 'a'|| v =='e'||v == 'i'||v=='o'||v=='u'||v=='A'||v=='E'||v=='I'||v=='O'||v=='U')
+    {
+        cout<<"It is a vowel.";
+    }
+    else
+    {
+        cout<<"It is a consonant.";
+    }
+}
