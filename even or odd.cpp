b7c817be42aa807e078debab8a9cd51d5f2f2a9f
aryan42bit <aryankulkarni6969@gmail.com>
2024-08-26 21:43:21 +0530

    Create even or odd.cpp

---
+++ even or odd.cpp
@@ -0,0 +1,20 @@
+//23070123076
+//aryankulkarni//
+#include<iostream>
+using namespace std;
+int main()
+
+{
+    int a;
+    cout<<"Enter the number: ";
+    cin>>a;
+
+    if(a%2 == 0)
+    {
+        cout<<"It is an even number.";
+    }
+    else
+    {
+        cout<<"It is an odd number.";
+    }
+}
